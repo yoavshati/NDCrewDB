9793c7792d28a8e658b78bfeccf1eeffc8f41f7d
yoavshati <yoavshati@gmail.com>
2021-06-02 09:55:13 +0300

    first attempt at mu;tiple parts per test doesn't really work

--- main/models.py
+++ main/models.py
@@ -4,6 +4,8 @@ from django.core.validators import RegexValidator
 from django.utils import timezone
 from .choices import *
 
+# adds fields to default user
+# lets admins track work hours
 class User(AbstractUser):
     full_name = models.CharField(default = '-', max_length = 50)
     personal_number = models.CharField(blank = True, max_length = 7, validators = [RegexValidator(regex = r'\d{7}')])
@@ -13,8 +15,30 @@ class User(AbstractUser):
     def __str__(self):
         return str(self.full_name)
 
+# saves item types for automatic field filling
+class ReferanceItem(models.Model):
+    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
+    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
+    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
+    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
+    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
+
+    def __str__(self):
+        return self.part_description
+
+# allows user to save multiple items
+# allows admins to modify specific items
+class TestItem(models.Model):
+    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
+    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
+    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
+    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
+    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
+
+    def __str__(self):
+        return self.part_description
+
 class Test(models.Model):
-    # might be useful to change choices to foreign key so admins can add choices without changing the code
     department = models.IntegerField(choices = DEPARTMENT, verbose_name = 'מחלקה')
     test_date = models.DateField(verbose_name = 'תאריך ביצוע הבדיקה')
     test_time_start = models.TimeField(verbose_name = 'שעת תחילת הבדיקה')
@@ -25,14 +49,7 @@ class Test(models.Model):
     aircraft_id = models.CharField(max_length = 50, verbose_name = "מס' זנב/סידורי")
     meter_id = models.CharField(max_length = 16, blank = True, verbose_name = "מס' סידורי של מכשיר הבדיקה")
 
-    # fields related to part
-    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
-    part_id = models.CharField(max_length = 16, blank = True, verbose_name = "מס' סידורי של הפריט")
-    manufacturer_id = models.CharField(max_length = 16, blank = True, verbose_name = 'מספר יצרן')
-    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
-    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
-    tag_number = models.CharField(max_length = 16 , blank = True, verbose_name = "מספר תג תהליך בדיקת אל הרס")
-    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
+    parts = models.ManyToManyField(TestItem) # unfinished. look up how to do properly
 
     findings = models.IntegerField(choices = FINDINGS, blank = True, verbose_name = 'ממצאים')
     notes = models.CharField(max_length = 50, blank = True, verbose_name = 'הערות')
@@ -54,24 +71,4 @@ class Test(models.Model):
 
     
     def __str__(self):
-        return 'בדיקת ' + str(self.get_test_method_display()) + ' על ' + str(self.part_description)
-
-class ReferanceItem(models.Model):
-    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
-    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
-    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
-    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
-    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
-
-    def __str__(self):
-        return self.part_description
-
-class TestItem(models.Model):
-    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
-    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
-    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
-    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
-    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
-
-    def __str__(self):
-        return self.part_description
+        return self.tester.full_name + ', ' + str(self.test_date)
