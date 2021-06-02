c66aefce60d620b35bd2d1eaa7c4b9da31fd9c95
yoavshati <yoavshati@gmail.com>
2021-06-02 13:30:03 +0300

    changed foreign key logic added wanted form to admin

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import *
 
+class ItemInline(admin.TabularInline):
+    model = TestItem
+
+class TestAdmin(admin.ModelAdmin):
+    model = Test
+    inlines = [ItemInline]
+
 admin.site.register(User)
-admin.site.register(Test)
+admin.site.register(Test, TestAdmin)
 admin.site.register(ReferanceItem)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from .choices import *
 
@@ -15,41 +16,18 @@ class User(AbstractUser):
     def __str__(self):
         return str(self.full_name)
 
-# saves item types for automatic field filling
-class ReferanceItem(models.Model):
-    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
-    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
-    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
-    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
-    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
-
-    def __str__(self):
-        return self.part_description
-
-# allows user to save multiple items
-# allows admins to modify specific items
-class TestItem(models.Model):
-    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
-    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
-    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
-    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
-    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
-
-    def __str__(self):
-        return self.part_description
-
 class Test(models.Model):
     department = models.IntegerField(choices = DEPARTMENT, verbose_name = 'מחלקה')
+    test_location = models.IntegerField(choices = LOCATION, verbose_name = 'מקום ביצוע הבדיקה')
+
     test_date = models.DateField(verbose_name = 'תאריך ביצוע הבדיקה')
     test_time_start = models.TimeField(verbose_name = 'שעת תחילת הבדיקה')
     test_time_end = models.TimeField(verbose_name = 'שעת סיום הבדיקה')
-    test_location = models.IntegerField(choices = LOCATION, verbose_name = 'מקום ביצוע הבדיקה')
+
     number_of_parts = models.IntegerField(default = 1, verbose_name = 'מספר פריטים בבדיקה')
     aircraft = models.CharField(max_length = 50, verbose_name = 'כלי טייס/מכלול')
     aircraft_id = models.CharField(max_length = 50, verbose_name = "מס' זנב/סידורי")
     meter_id = models.CharField(max_length = 16, blank = True, verbose_name = "מס' סידורי של מכשיר הבדיקה")
-
-    parts = models.ManyToManyField(TestItem) # unfinished. look up how to do properly
 
     findings = models.IntegerField(choices = FINDINGS, blank = True, verbose_name = 'ממצאים')
     notes = models.CharField(max_length = 50, blank = True, verbose_name = 'הערות')
@@ -68,7 +46,31 @@ class Test(models.Model):
         related_query_name = "checker",
         verbose_name = 'בודק'
     )
-
     
     def __str__(self):
         return self.tester.full_name + ', ' + str(self.test_date)
+
+# saves item types for automatic field filling
+class ReferanceItem(models.Model):
+    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
+    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
+    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
+    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
+    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
+
+    def __str__(self):
+        return self.part_description
+
+# allows user to save multiple items
+# allows admins to modify specific items
+class TestItem(models.Model):
+    test = models.ForeignKey(Test, on_delete = models.CASCADE)
+
+    part_description = models.CharField(max_length = 50, verbose_name = 'תיאור פריט')
+    manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
+    test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
+    literature_code = models.CharField(max_length = 50, verbose_name = 'שיוך לספרות טכנית')
+    testing_hours = models.FloatField(verbose_name = 'שעתון בדיקה')
+
+    def __str__(self):
+        return self.part_description
