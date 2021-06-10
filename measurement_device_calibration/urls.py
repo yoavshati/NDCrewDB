2f2a96f34c3b024efb826f2cfb94abba94f6bd4d
yoavshati <yoavshati@gmail.com>
2021-06-10 16:03:34 +0300

    created pages for measure device app

--- measurement_device_calibration/urls.py
+++ measurement_device_calibration/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', MeasurementDeviceListView.as_view(), name='item-list'),
-    path('new/', MeasurementDeviceCreateView.as_view(), name='item-create'),
-    path('update/<int:pk>/', MeasurementDeviceUpdateView.as_view(), name='item-update'),
-    path('delete/<int:pk>/', MeasurementDeviceDeleteView.as_view(), name='item-delete'),
+    path('', MeasurementDeviceListView.as_view(), name='md-list'),
+    path('new/', MeasurementDeviceCreateView.as_view(), name='md-create'),
+    path('update/<int:pk>/', MeasurementDeviceUpdateView.as_view(), name='md-update'),
+    path('delete/<int:pk>/', MeasurementDeviceDeleteView.as_view(), name='md-delete'),
     ]
