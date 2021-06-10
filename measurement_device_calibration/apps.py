8e1e33d4ce710c0e15802e79aeb8517248dc9834
yoavshati <yoavshati@gmail.com>
2021-06-10 14:56:15 +0300

    started measurement device app

---
+++ measurement_device_calibration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MeasurementDeviceCalibrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'measurement_device_calibration'
