b43ecc5dfcfb01b16d8d6b809dd889c571ce8d06
yoavshati <yoavshati@gmail.com>
2021-06-10 15:09:02 +0300

    removed unused import

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from .choices import *
 
