ee6e365cc5df769a8c22b066e0585ccf9d95c738
yoavshati <yoavshati@gmail.com>
2021-06-14 08:27:02 +0300

    changed app name

--- NDCrewDB/settings.py
+++ NDCrewDB/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'measurement_device_calibration',
+    'measurement device calibration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
