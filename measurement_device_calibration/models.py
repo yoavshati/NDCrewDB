ddd4ecee412fd901959e74e30727d59ef00391fd
yoavshati <yoavshati@gmail.com>
2021-06-14 13:17:25 +0300

    changed measuring device model

--- measurement_device_calibration/models.py
+++ measurement_device_calibration/models.py
@@ -6,7 +6,7 @@ class MeasurementDevice(models.Model):
     device_name = models.CharField(max_length = 50, verbose_name = 'שם פריט')
     manufacturer_id = models.CharField(max_length = 50, verbose_name = 'מספר יצרן')
     AF_id = models.CharField(max_length = 50, verbose_name = 'מסח"א')
-    measurement_device_id = models.CharField(max_length = 16, verbose_name = "מס' סידורי של מכשיר הבדיקה")
+    device_id = models.CharField(max_length = 16, verbose_name = "מס' סידורי של מכשיר הבדיקה")
     # test_method = models.IntegerField(choices = TEST_METHOD, verbose_name = 'שיטת בדיקה')
     calibration_date = models.DateField(default = timezone.now, verbose_name = 'תאריך כיול אחרון')
     expiration_date = models.DateField(verbose_name = 'בתוקף עד')
