144bb691de8c36a4592cee893a2b612dec7e42ad
yoavshati <yoavshati@gmail.com>
2021-06-14 08:28:19 +0300

    reversed faulty addition

--- NDCrewDB/settings.py
+++ NDCrewDB/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    'measurement device calibration',
+    'measurement_device_calibration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
